5a7505ca6471d3c3b6012d4fffaf1d205dd3567f
Marco Nardone <marconardone97@gmail.com>
2021-05-17 14:47:08 +0200

    Multiple NICs support!
    You can now choose on which network interface bind the software (ONVIF probe and webserver).

--- h2o.js
+++ h2o.js
@@ -3,12 +3,31 @@ const Stream = require('./src/node-rtsp-stream-jsmpeg.js')
 const http = require('http');
 const url = require('url');
 const fs = require('fs');
+const readline = require("readline");
 var ip = require("ip");
+var os_nics = require('os').networkInterfaces();
 var tb = require("console.table");
 const fileHTML = fs.readFileSync('test/controls.html');
 var r = /\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b/;
 global.pippo = {};
 var loading;
+var myArgs = process.argv.slice(2);
+
+var ifname = "";
+
+switch (myArgs[0]) {
+case '-i':
+	ifname = myArgs[1];
+    break;
+default:
+    console.log('Sorry, that is not something I know how to do.');
+}
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
 console.log(` __________
 < Si vede? >
  ----------
@@ -20,6 +39,72 @@ console.log(` __________
 
 console.log("\n------------------------------------");
 console.log("H2O - HTTP to ONVIF Proxy Server 1.0");
+
+delete os_nics.lo;
+//delete os_nics.tun0;
+
+if(Object.keys(os_nics).length > 1 && ifname == ""){
+
+	//console.log("\nMultiple network intefaces detected, which one do you want me to use?\n");
+
+	const interfaces = [];
+
+	for(var i = 0; i < Object.keys(os_nics).length; i++){
+		var ifname = Object.keys(os_nics)[i];
+		var ip_address = getIpFromInterfaceName(ifname);
+		var temp = [];
+		temp["Interface name"] = ifname;
+		temp["Ip address"] = ip_address;
+		interfaces.push(temp);
+	}
+
+	console.table("\nMultiple network intefaces detected, which one do you want me to use?\n", interfaces);
+
+	rl.question("Network interface name: ", function(name) {
+		main(name.toLowerCase());
+	    rl.close();
+	});
+
+}else{
+	if(ifname == ""){
+		ifname = Object.keys(os_nics)[0];
+	}
+	main(ifname);
+}
+
+function getIpFromInterfaceName(ifname){
+	for(var i = 0; i < Object.keys(os_nics).length; i++){
+		if(ifname == Object.keys(os_nics)[i]){
+			var ip_address = Object.values(os_nics)[i][0]['address'];
+			return ip_address;
+		}
+	}
+}
+
+function loading() {
+	loading = (function () {
+		var h = ['|', '/', '-', '\\'];
+		var i = 0;
+
+		return setInterval(() => {
+			i = (i > 3) ? 0 : i;
+			process.stdout.write('Searching for Onvif devices: ' + h[i] + '\r');
+			i++;
+		}, 300);
+	})();
+}
+
+function main(name){
+
+const callback = () => {
+	const address = server.address().address;
+	const port = server.address().port;
+	console.log('\x1b[33m%s\x1b[0m', "Server started at http://"+address+":"+port)
+}
+
+const bound_address = getIpFromInterfaceName(name);
+
+console.log(`Interface \"${name}\" selected, binding to ${bound_address}\n`)
 
 global.direction_lib = {
 	"up": {
@@ -87,21 +172,27 @@ global.connected = false;
 
 var discover_result = {};
 loading();
-onvif.startProbe().then((device_list) => {
-	clearInterval(loading);
-	device_list.forEach((device) => {
-		var cam_xaddr = device.xaddrs[0];
-		var cam_ip = cam_xaddr.match(r);
-		discover_result[cam_ip] = cam_xaddr;
+
+	onvif.startProbe({bind_address: bound_address}).then((device_list) => {
+		clearInterval(loading);
+		var discover_result_table = [];
+		device_list.forEach((device) => {
+			var cam_xaddr = device.xaddrs[0];
+			var cam_ip = cam_xaddr.match(r);
+			var temp = [];
+			temp["Cam IP"] = cam_xaddr.match(r)
+			temp["XADDR"] = device.xaddrs[0];
+			discover_result_table.push(temp);
+			discover_result[cam_ip] = cam_xaddr;
+		});
+
+		console.table("Onvif device scan result: " + device_list.length + " device(s) found:\n", discover_result_table);
+		console.log("\x1b[32mREADY\x1b[0m\n");
+	}).catch((error) => {
+		console.error(error);
 	});
 
-	console.table("Onvif device scan result: " + device_list.length + " device(s) found:\n", discover_result);
-	console.log("\x1b[32mREADY\x1b[0m\n");
-}).catch((error) => {
-	console.error(error);
-});
-
-const server = http.createServer((req, res) => {
+	global.server = http.createServer((req, res) => {
 
 	if (req.url != '/favicon.ico') {
 		params = url.parse(req.url, true).query;
@@ -181,28 +272,9 @@ const server = http.createServer((req, res) => {
 	res.end(fileHTML);
 });
 
-const callback = () => {
-	const address = server.address().address;
-	const port = server.address().port;
-	console.log('\x1b[33m%s\x1b[0m', "Server started at http://"+address+":"+port)
-}
-
-server
-	.listen(
+server.listen(
 		8070,
-		ip.address(),
+		bound_address,
 		callback
 	)
-
-function loading() {
-	loading = (function () {
-		var h = ['|', '/', '-', '\\'];
-		var i = 0;
-
-		return setInterval(() => {
-			i = (i > 3) ? 0 : i;
-			process.stdout.write('Searching for Onvif devices: ' + h[i] + '\r');
-			i++;
-		}, 300);
-	})();
 }
--- install.sh
+++ install.sh
@@ -1,11 +1,64 @@
 #!/bin/bash
 
-apt-get update && apt-get -y install ffmpeg nodejs npm
+function installsyspkg(){
 
-npm install ws
-npm install node-onvif
-npm install ip
-npm install console.table
-npm install http-shutdown
+	PACKAGE="$1"
 
+	echo -ne "Installing $PACKAGE..."
+	apt-get -y install "$PACKAGE" > /dev/null 2>&1
 
+	if [ $? -ne 0 ]; then
+	    echo "FAIL"
+	    exit 1
+	else
+		echo "done"
+	fi	
+
+}
+
+function installnodepkg(){
+
+	PACKAGE="$1"
+
+	echo -ne "Installing package $PACKAGE..."
+	npm install "$PACKAGE" > /dev/null 2>&1
+
+	if [ $? -ne 0 ]; then
+	    echo "FAIL"
+	    exit 1
+	else
+		echo "done"
+	fi	
+
+}
+
+echo "This script will install all the required software to run h2o.js"
+read -p "Press enter to continue or CTRL+C to abort"
+
+echo "Running apt update..."
+apt-get update > /dev/null 2>&1
+
+if [ $? -ne 0 ]; then
+    echo "There was an error running apt update, exiting"
+    exit 1
+else
+	echo "Packages list updated"
+fi
+
+installsyspkg ffmpeg
+installsyspkg nodejs
+installsyspkg npm
+
+echo "All required system packages have been installed, installing node packages..."
+
+installnodepkg ws
+installnodepkg node-onvif
+installnodepkg ip
+installnodepkg console.table
+installnodepkg http-shutdown
+
+echo "Replacing node-onvif.js..."
+
+mv src/node-onvif.js node_modules/node-onvif/lib/
+
+echo "DONE"
